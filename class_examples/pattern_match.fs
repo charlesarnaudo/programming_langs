75abd77f2286454f29b46a58077612589626658a
charles <charlesarnaudo@gmail.com>
2018-05-23 12:49:57 -0400

    convert to pattern matching func

---
+++ class_examples/pattern_match.fs
@@ -0,0 +1,22 @@
+// Non pattern matched
+// let f x y =
+//      if y = 0 then x
+//      else if x = 0 then y
+//      else x / y;;
+let f x y = 
+    match (x, y) with
+    | (x, 0) -> x
+    | (0, y) -> y
+    | (_, _) -> x / y
+
+// Non pattern matched
+// let f (L : int list) =
+//      if L.IsEmpty then []
+//      else if L.Tail.IsEmpty then L
+//      else [L.Tail.Head]
+
+let f2 (L : int list) =
+    match L with 
+    | [] -> []
+    | h::[] -> L
+    | h::t -> [t.Head]
