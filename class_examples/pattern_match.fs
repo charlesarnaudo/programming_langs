690aac3dafc685d5d42aa1960708443f6e9ed3c9
charles <charlesarnaudo@gmail.com>
2018-05-23 13:11:20 -0400

    done

--- class_examples/pattern_match.fs
+++ class_examples/pattern_match.fs
@@ -1,15 +1,13 @@
-// Non pattern matched
 // let f x y =
 //      if y = 0 then x
 //      else if x = 0 then y
-//      else x / y;;
+//      else x / y
 let f x y = 
     match (x, y) with
     | (x, 0) -> x
     | (0, y) -> y
     | (_, _) -> x / y
 
-// Non pattern matched
 // let f (L : int list) =
 //      if L.IsEmpty then []
 //      else if L.Tail.IsEmpty then L
@@ -19,3 +17,27 @@ let f2 (L : int list) =
     | [] -> []
     | h::[] -> L
     | h::t -> [t.Head]
+
+// let rec snoc a (L : 'a list) =
+//   if L.IsEmpty then [a]
+//   else L.Head::snoc a L.Tail
+let rec snoc a (L: 'a list) =
+    match L with 
+    | [] -> [a]
+    | _ -> L.Head::(snoc a t)
+
+//let rec rac (L : ’a list) =
+//    if L.Tail.IsEmpty then L.Head
+//    else rac L.Tail
+let rec rac (L : 'a list) =
+    match L with 
+    | h::[] -> h
+    | _ -> rac L.Tail
+
+// let rec rdc (L : ’a list) =
+//     if L.Tail.IsEmpty then []
+//     else L.Head::rdc(L.Tail);;
+let rec rdc (L : 'a list) =
+    match L with 
+    | h::[] -> h
+    | _ -> h::(rdc L.Tail)
