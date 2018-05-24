501c2b2840a0737e0b15bb41992925f314a508a7
charles <charlesarnaudo@gmail.com>
2018-05-24 11:24:38 -0400

    lab 2

---
+++ labs/lab_2.fs
@@ -0,0 +1,18 @@
+﻿namespace LAB2
+
+module LAB2 =    
+    // Note: you need to change this function signature to use patterns
+    // Takes two tuples and returns them added
+    // If either the first or second tuple (but not both) are a pair of 0s, return other tuple
+    // otherwise, return tuples added
+    let addTuples ((a, b): int * int) ((c, d): int * int) =
+        match ((a, b), (c, d)) with
+        | ((0, 0), _) -> (c, d)
+        | (_, (0, 0)) -> (a, b)
+        | (_, _) -> (a+c, b+d)
+    
+    // Takes a Function f and paramter x for that func
+    // Calls f twice
+    let dup f x =
+        f (f x)
+    
