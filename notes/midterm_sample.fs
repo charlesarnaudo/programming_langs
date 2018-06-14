5c76bc24810abb2de37b7f66a8afbf7978c7c01c
charles <charlesarnaudo@gmail.com>
2018-06-14 13:26:22 -0400

    some midterm example problems

--- notes/midterm_sample.fs
+++ notes/midterm_sample.fs
@@ -1,7 +1,6 @@
 type irs = I of int | R of double | S of string | IR of int * double | IS of int * string
+type 'e mylist = NIL | CONS of 'e * 'e mylist
 
-
-            
 let count5 L =
     let rec count5helper L n = 
         match L with
@@ -10,6 +9,28 @@ let count5 L =
     in
         count5helper L 0
 
-let lmult x y =
+let imult x y =
     match (x, y) with
     | (I a), (I b) -> I (a * b)
+
+let IxS i s =
+    match (i, s) with
+    | (I i), (S s) -> IS (i, s)    
+
+let myLength (L : 'e mylist) =
+    let rec myLengthHelper L n =
+        match L with
+        | NIL -> 0
+        | CONS (h, t) -> myLengthHelper t n+1
+    in
+        myLengthHelper L 0
+
+let rec rdc (L :'a list) =
+    match L with 
+    | h::[] -> []
+    | h::t -> h::(rdc L.Tail)
+
+let rec tailRdc a (b : 'a list) =
+    match b with 
+    | [h] -> a
+    | h::t ->  tailRdc (h::a) t
